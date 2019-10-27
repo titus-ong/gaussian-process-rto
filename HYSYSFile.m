42939d2cdc3496e9cdacb692d7877166d51db8d7
Titus Ong <55629508+titus-ong@users.noreply.github.com>
2019-10-27 01:42:42 +0100

    Update cell reference and objective function

--- HYSYSFile.m
+++ HYSYSFile.m
@@ -1,27 +1,45 @@
 classdef HYSYSFile
     properties (SetAccess=private)
-        simulation       % Simulation object
-        flowsheet        % Simulation flowsheet
-        operations       % Simulation operations
-        solver           % Simulation solver
-        spreadsheet      % Simulation spreadsheet
+        simulation                                 % Simulation object
+        flowsheet                                  % Simulation flowsheet
+        operations                                 % Simulation operations
+        solver                                     % Simulation solver
+        spreadsheet                                % Simulation spreadsheet
     end
     properties (Constant)
-        cells = struct( ...
-            'solvent_flowrate', 'C2', ...
-            'reboiler_duty', 'C3', ...
-            'co2_recovery', 'C4' ...
-            );           % Cell struct of spreadsheet
+        cells = struct( ...                        % Cell struct of spreadsheet
+            'solvent_flowrate', {'A1', 'B1'}, ...  % Var_name, {input, output}
+            'reboiler_duty', {'A2', 'B2'}, ...
+            'inlet_co2_comp', {'A3', 'B3'}, ...
+            'inlet_gas_flowrate', {'A4', 'B4'}, ...
+            'clean_gas_co2', {'A5', 'B5'}, ...
+            'p_absorber', {'A6', 'B6'}, ...
+            'pout_absorber', {'A7', 'B7'}, ...
+            'p_stripper', {'A8', 'B8'}, ...
+            'pout_stripper', {'A9', 'B9'}, ...
+            't_condenser', {'A10', 'B10'}, ...
+            't_mea_recycle', {'A11', 'B11'}, ...
+            't_inlet_stripper', {'A12', 'B12'}, ...
+            't_inlet_gas', {'A13', 'B13'}, ...
+            'recycle_co2', {'A14', 'B14'}, ...
+            'recycle_mea', {'A15', 'B15'}, ...
+            'recycle_h2o', {'A16', 'B16'}, ...
+            'e_cooling_water', {'A17', 'B17'}, ...
+            'e_c100', {'A18', 'B18'}, ...
+            'e_c200', {'A19', 'B19'}, ...
+            'e_j100', {'A20', 'B20'}, ...
+            'e_j101', {'A21', 'B21'}, ...
+            'objective_true', 'A22' ...
+            );
         feasible_point = struct( ...
-            "reboiler_duty", 170000, ...
+            "reboiler_duty", 154400, ...
             "solvent_flowrate", 250 ...
             );
-        delta = struct( ...  10% of operating range
+        delta = struct( ...                    % 10% of operating range
             "reboiler_duty", 300, ...
             "solvent_flowrate", 15 ...
             );
-        output_fields = ["co2_recovery"];
-        objective = @(x) x(1);                 % Minimise reboiler duty
+        output_fields = ["objective_true"];
         linear_con_A = [];                     % Linear inequality constraints LHS (Ax = b)
         linear_con_b = [];                     % Linear inequality constraints LHS
         lineq_con_A = [];                      % Linear equality constraints LHS
@@ -31,9 +49,10 @@ classdef HYSYSFile
         options = optimset('disp','iter');     % Options for GP
     end
     properties
-        feasible_point_mat % Matrix form
-        delta_mat          % Matrix form
-        nonlin_con         % Nonlinear constraints
+        feasible_point_mat                     % Matrix form
+        delta_mat                              % Matrix form
+        nonlin_con                             % Nonlinear constraints
+        objective                              % Minimise reboiler duty
     end
     methods
         function obj = HYSYSFile(filepath, spreadsheet_name) % Constructor method
@@ -47,35 +66,48 @@ classdef HYSYSFile
             obj.feasible_point_mat = cell2mat(struct2cell(obj.feasible_point))';
             obj.delta_mat = cell2mat(struct2cell(obj.delta))';
             obj.nonlin_con = @(x, centre, delta, model, mean_x, std_x, mean_y, std_y) ...
-            obj.nonlinfn(x, centre, delta, model, mean_x, std_x, mean_y, std_y);
+                obj.nonlin_fn(x, centre, delta, model, mean_x, std_x, mean_y, std_y);
+            obj.objective = @(x, model, mean_x, std_x, mean_y, std_y) ...
+                obj.obj_fn(x, model, mean_x, std_x, mean_y, std_y);
         end
         
-        function [c,ceq] = nonlinfn(obj, x, centre, delta, model, mean_x, std_x, mean_y, std_y)
+        function [c,ceq] = nonlin_fn(obj, x, centre, delta, model, mean_x, std_x, mean_y, std_y)
             % Nonlinear inequality (c<=0) and equality (ceq=0) constraints on x
             
             % Point is within delta
             c(1) = sum((x - centre).^2) - sum((delta).^2);
             
             % CO2 recovery > 0.99
-            c(2) = obj.co2_fn(x, mean_x, std_x, mean_y, std_y, model);
+            % TO BE ADDED WHEN CO2 WORKS IN HYSYS
+            c(2) = obj.co2_fn(x, model, mean_x, std_x, mean_y, std_y);
             
             ceq = [];
         end
         
-        function recovery = co2_fn(~, x, mean_x, std_x, mean_y, std_y, model)
+        function recovery = co2_fn(~, x, model, mean_x, std_x, mean_y, std_y)
             predicted = predict(model, (x - mean_x) ./ std_x) .* std_y + mean_y;
-            recovery = 0.99 - predicted(1);
+            recovery = predicted(1) - 1;  % Replace (1) to find co2 recovery according to output field
+        end
+        
+        function objective = obj_fn(~, x, model, mean_x, std_x, mean_y, std_y)
+            switch nargin
+                case 2  % True objective - no model inputs
+                    [~, outputs] = obj.get_output(x);
+                otherwise  % Model objective
+                    outputs = predict(model, (x - mean_x) ./ std_x) .* std_y + mean_y;
+            end
+            objective = outputs(1);  % Change (1) according to objective required
         end
         
         function value = get_param(obj, parameter)
             % Get parameter from HYSYS spreadsheet
-            ref = obj.cells.(parameter);
+            ref = obj.cells(2).(parameter);
             value = obj.spreadsheet.Cell(ref).CellValue;
         end
         
         function set_param(obj, parameter, value)
             % Set parameter on HYSYS spreadsheet
-            ref = obj.cells.(parameter);
+            ref = obj.cells(1).(parameter);
             obj.spreadsheet.Cell(ref).CellValue = value;
         end
         
