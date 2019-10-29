ac643361685370bf4cb22a90039cc56c491d8a66
Titus Ong <55629508+titus-ong@users.noreply.github.com>
2019-10-29 15:35:14 +0000

    Include user name variables section for clearer formatting

--- example_hysys.m
+++ example_hysys.m
@@ -1,7 +1,12 @@
+%% User name variables
+hysys_name = "\testing.hsc";
+spreadsheet_input = "Inputs";
+spreadsheet_output = "Outputs";
+
 %% Connect to HYSYS
 tic
-hysys_path = pwd + "\testing.hsc";
-hysys = HYSYSFile(hysys_path, "SPRDSHT-1");
+hysys_path = pwd + hysys_name;
+hysys = HYSYSFile(hysys_path, spreadsheet_input, spreadsheet_output);
 toc
 
 %% Initialising Points for Training
