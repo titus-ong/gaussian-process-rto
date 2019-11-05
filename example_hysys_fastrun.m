bfbee77380080dfd8bae3b4f26354159a7c9ec31
Huang Guan <gh1514@ic.ac.uk>
2019-11-05 00:30:35 +0000

    New matlab files for fast run

---
+++ example_hysys_fastrun.m
@@ -0,0 +1,25 @@
+%% User name variables
+hysys_name = "\fast run.hsc";
+spreadsheet_input = "Inputs";
+spreadsheet_output = "Outputs";
+
+%% Connect to HYSYS
+tic
+hysys_path = pwd + hysys_name;
+hysys = HYSYSFile_fastrun(hysys_path, spreadsheet_input, spreadsheet_output);
+toc
+
+%% Initialising Points for Training
+pointer = Pointer(hysys.feasible_point_mat, hysys.delta_mat);
+sample_points = pointer.random_sampling(5);
+toc
+[outputs, outputs_struct] = hysys.get_output(sample_points);
+toc
+
+GP = GPCreator(hysys, sample_points, outputs);
+toc
+
+%% Optimisation
+iter = 10;
+GP.optimise(iter);
+GP.plot();
