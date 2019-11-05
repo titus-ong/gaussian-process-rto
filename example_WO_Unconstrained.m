4396de7a1d685157f81bf1abaac4b337cb012d38
Titus Ong <55629508+titus-ong@users.noreply.github.com>
2019-11-05 22:50:46 +0000

    Include sample points

--- example_WO_Unconstrained.m
+++ example_WO_Unconstrained.m
@@ -5,18 +5,18 @@ WO = WilliamsOttoUnconstrained();
 
 %% Initialising Points for Training
 pointer = Pointer(WO.feasible_point_mat, WO.delta_mat);
-sample_points = pointer.random_sampling();
-% Fb = [6.9000, 6.7675, 7.0641, 7.1481, 6.9861, 6.6515]';
-% Tr = [83.0000, 85.0262, 83.6761, 81.5130, 79.5847, 80.4423]';
-% sample_points = [Fb, Tr];
+% sample_points = pointer.random_sampling();
+Fb = [6.9000, 6.7675, 7.0641, 7.1481, 6.9861, 6.6515]';
+Tr = [83.0000, 85.0262, 83.6761, 81.5130, 79.5847, 80.4423]';
+sample_points = [Fb, Tr];
 [outputs, outputs_struct] = WO.get_output(sample_points);
 
 GP = GPCreator(WO, sample_points, outputs);
 
 %% Optimisation
-for i = 1:50
+for i = 1:20
     GP.optimise(1);
-%     GP.system.time = GP.system.time + 1;
+    GP.system.time = GP.system.time + 1;
 end
 toc
 GP.plot();
