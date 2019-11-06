a2c598ac38713a169d4919d6a111afd09196594e
Titus Ong <55629508+titus-ong@users.noreply.github.com>
2019-11-06 02:19:55 +0000

    Change axes to fit with GP plot

--- op_region_plot_hysys.m
+++ op_region_plot_hysys.m
@@ -1,10 +1,10 @@
-excel = "\fast run results - Copy.xlsx";
+excel = "\fast run results.xlsx";
 rows = 21;  % No. of intervals of reboiler_duty
 cols = 21;  % No. of intervals of sour_gas
 
 data_grid = readmatrix(pwd + excel);
-x = linspace(40, 300, 21);
-y = linspace(80000, 200000, 21);
+x = linspace(80000, 200000, 21);
+y = linspace(40, 300, 21);
 
 % Objective
 z1 = plot_columns(data_grid, rows, cols, 27);
@@ -17,6 +17,11 @@ z2 = plot_columns(data_grid, rows, cols, 7);
 
 figure
 hold on;
+surf(x, y, z1)
+h = scatter3(x(col),y(row),z1(i),'r', 'filled');
+
+figure
+hold on;
 contour(x, y, z1, 20)
 scatter(x(col),y(row),'rx');
 % Draw CO2% contour
@@ -26,11 +31,6 @@ clabel(one_perc, h)
 clabel(two_perc, h)
 [three_perc, h] = contour(x, y, z2, [0.03 0.03]);
 clabel(three_perc, h)
-
-figure
-hold on;
-surf(x, y, z1)
-h = scatter3(x(col),y(row),z1(i),'r', 'filled');
 
 function [z] = plot_columns(data_grid, rows, cols, column)
 z = zeros(rows, cols);
