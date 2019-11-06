2b3c93d4093a9223c0e3d77040e1181079f3885d
Huang Guan <gh1514@ic.ac.uk>
2019-11-06 00:36:05 +0000

    updated with constraint GP

--- example_peaks.m
+++ example_peaks.m
@@ -5,7 +5,11 @@ Pks = PeaksFunction();
 %% Initialising Points for Training
 pointer = Pointer(Pks.feasible_point_mat, Pks.delta_mat);
 sample_points = pointer.random_sampling();
-[outputs, outputs_struct] = Pks.get_output(sample_points);
+[objective, con_ineq, con_eq] = Pks.get_output(sample_points);
+outputs = struct();
+outputs.objective = objective;
+outputs.con_ineq = con_ineq;
+outputs.con_eq = con_eq;
 
 GP = GPCreator(Pks, sample_points, outputs);
 
