73d068818770dbb34c6879e07d0d902610cab723
Huang Guan <gh1514@ic.ac.uk>
2019-11-06 00:35:49 +0000

    Updated with new constraint GP

--- PeaksFunction.m
+++ PeaksFunction.m
@@ -4,11 +4,13 @@ classdef PeaksFunction  < matlab.mixin.Copyable
             "x", -0.33, ...
             "y", 1 ...
             );
-        delta = struct( ...
+        delta_point = struct( ...
             "x", 0.6, ...
             "y", 0.6 ...
             );
         output_fields = {'z'};
+        constraints_ineq = [];
+        constraints_eq = [];
         linear_con_A = [];                         % Linear inequality constraints LHS (Ax = b)
         linear_con_b = [];                         % Linear inequality constraints LHS
         lineq_con_A = [];                          % Linear equality constraints LHS
@@ -20,80 +22,111 @@ classdef PeaksFunction  < matlab.mixin.Copyable
     properties
         feasible_point_mat                         % Matrix form
         delta_mat                                  % Matrix form
-        nonlin_con                                 % Nonlinear constraints
-        objective_model                            % Objective function for model
-        objective_true                             % Objective function for system
-        system_violation                           % Test for system constraint violation
         decay                                      % Decay boolean
         time                                       % Pseudo time for decay
+        op_region_script                           % Functionf or plotting operation region
     end
     methods
         function obj = PeaksFunction()
             obj.feasible_point_mat = cell2mat(struct2cell(obj.feasible_point))';
-            obj.delta_mat = cell2mat(struct2cell(obj.delta))';
-            obj.nonlin_con = @(x, centre, delta, model, mean_x, std_x, mean_y, std_y) ...
-                obj.nonlin_fn(x, centre, delta, model, mean_x, std_x, mean_y, std_y);
-            obj.objective_model = @(x, model, mean_x, std_x, mean_y, std_y) ...
-                obj.model_obj_fn(x, model, mean_x, std_x, mean_y, std_y);
-            obj.objective_true = @(x) obj.true_obj_fn(x);
-            obj.system_violation = @(y) obj.system_violated(y);
+            obj.delta_mat = cell2mat(struct2cell(obj.delta_point))';
             obj.decay = false;
             obj.time = 0;
+            obj.op_region_script = @op_region_plot_peaks;
         end
         
-        function [c,ceq] = nonlin_fn(~, x, centre, delta, model, mean_x, std_x, mean_y, std_y)
-            % Nonlinear inequality (c<=0) and equality (ceq=0) constraints on x
-            
-            % Point is within delta
-            c(1) = sum((x - centre).^2 ./ delta.^2) - 1;
+        function objective = objective_value(obj, inputs, outputs)
+            % Calculate objective function
+            objective = outputs;
 
-            ceq = [];
+        end
+
+        function obj = time_increment(obj)
+            if obj.decay
+                obj.time = obj.time + 1;
+            end
         end
         
-        function bool = system_violated(obj, output)
+        function [c,ceq] = nonlin_con(obj, x, par)  % centre, delta, model, mean_x, std_x, mean_y, std_y)
+            % Nonlinear inequality (c<=0) and equality (ceq=0) constraints on x
+            c = zeros(1 + length(obj.constraints_ineq), 1);
+            
+            % Inequality constraints
+            for i = 1:length(obj.constraints_ineq)
+                c(i) = predict(par.model.(obj.constraints_ineq{i}), ( ...
+                    x - par.values_adj.input.mean) ./ par.values_adj.input.std ...
+                    ) ...
+                    * par.values_adj.(obj.constraints_ineq{i}).std ...
+                    + par.values_adj.(obj.constraints_ineq{i}).mean;
+            end
+            
+            % Point is within delta - must be the final equation!
+            c(end) = sum((x - par.centre) .^ 2 ./ par.delta .^ 2) - 1;
+            
+            ceq = zeros(length(obj.constraints_eq), 1);
+            % Equality constraints
+            for i = 1:length(obj.constraints_eq)
+                ceq(i) = predict(par.model.(obj.constraints_eq{i}), ( ...
+                    x - par.values_adj.input.mean) ./ par.values_adj.input.std ...
+                    ) ...
+                    * par.values_adj.(obj.constraints_eq{i}).std ...
+                    + par.values_adj.(obj.constraints_eq{i}).mean;
+            end
+        end
+        
+        function par = create_par(~, GPobj, idx)
+            % Create parameters for fmincon (used in nonlin_fn)
+            par.model = GPobj.model(end);
+            par.values_adj = GPobj.values_adj;
+            par.centre = GPobj.centre(idx, :);
+            par.delta = GPobj.delta(idx, :);
+%             par.delta_norm = cell2mat(struct2cell(obj.delta_norm))';
+        end
+        
+        function bool = system_violated(~, con_ineq, con_eq)
             % Test if output violates system constraints
             % Return true if violated, false if not
+            tol = 1e-4;
             bool = false;
-        end
-        
-        function objective = model_obj_fn(obj, x, model, mean_x, std_x, mean_y, std_y)
-            % Calculate objective function for model
-            outputs = zeros(1, length(obj.output_fields));
-            for i = 1:length(obj.output_fields)
-%                 outputs(i) = predict(model(i), (x - mean_x) ./ std_x) .* std_y(i) + mean_y(i);
-                outputs(i) = predict(model(end).(obj.output_fields{i}), x);
+            
+            % Check inequality constraints
+            for i = 1:length(con_ineq)
+                if con_ineq(i) > 0
+                    bool = true;
+                end
             end
-            objective = obj.calc_objective(outputs);
+            
+            % Check equality constraints
+            for i = 1:length(con_eq)
+                if abs(con_eq(i)) > tol
+                    bool = true;
+                end
+            end
         end
         
-        function objective = true_obj_fn(obj, x)
-            % Calculate objective function for system
-            [outputs, ~] = obj.get_output(x);
-            objective = obj.calc_objective(outputs);
-        end
         
-        function objective = calc_objective(obj, outputs)
-            % Calculate objective function from outputs
-            objective = outputs(1);
-        end
-        
-        function [outputs, outputs_struct] = get_output(obj, inputs)
-            % Get output values from HYSYS spreadsheet using given inputs
-            outputs_struct = struct();
-
-            % Logical arrays for getting indices
-            x = (fieldnames(obj.feasible_point)=="x");
-            y = (fieldnames(obj.feasible_point)=="y");
+        function [objective, con_ineq, con_eq] = get_output(obj, inputs)
+            % Get objective function and constraint values using given inputs
+            n = size(inputs, 1);
+            m = length(obj.constraints_ineq);
+            k = length(obj.constraints_eq);
+            objective = zeros(n, 1);
+            con_ineq = zeros(n, m);
+            con_eq = zeros(n, k);
             
             % Solve reactor
-            for point = 1:size(inputs, 1)
+            for point = 1:n
                 curr_inputs = inputs(point, :);
-                z = peaks(curr_inputs(x), curr_inputs(y));
-                outputs_struct(point).z = z;
-            end
-            outputs = squeeze(cell2mat(struct2cell(outputs_struct)));
-            if size(outputs, 2) ~= length(obj.output_fields)
-                outputs = outputs';
+                z = peaks(curr_inputs(1), curr_inputs(2));
+                objective(point) = obj.objective_value(curr_inputs, z);
+                for idx = 1:m
+                    val = obj.(obj.constraints_ineq{idx})(curr_inputs, z);
+                    con_ineq(point, idx) = val;
+                end
+                for idx = 1:k
+                    val = obj.(obj.constraints_eq{idx})(curr_inputs, z);
+                    con_eq(point, idx) = val;
+                end
             end
         end
     end
