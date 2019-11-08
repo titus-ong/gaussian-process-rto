583d668be295daf3c7e229f9244698d3a21a221a
Huang Guan <gh1514@ic.ac.uk>
2019-11-08 18:11:03 +0000

    Added constraint and tried to plot the constraint GP

--- op_region_plot_hysys_GP.m
+++ op_region_plot_hysys_GP.m
@@ -4,7 +4,7 @@ cols = 31;
 x1 = linspace(hysys.lb(1), hysys.ub(1), cols);
 x2 = linspace(hysys.lb(2), hysys.ub(2), rows);
 temp_GP = copy(GP);
-model_idx = 11;
+model_idx = 3;
 temp_GP.model = temp_GP.model(1:model_idx);
 
 data = zeros(rows, cols);
@@ -14,7 +14,25 @@ for i = 1:cols
     end
 end
 
+data1 = zeros(rows, cols);
+for i = 1:cols
+    for j = 1:rows
+        data1(j, i) = constraint(temp_GP,[x1(i), x2(j)]);
+    end
+end
+
 f = figure;
 hold on;
-surf(x1, x2, data)
+%surf(x1, x2, data)
+%contour(x1, x2, data1,30)
+[one_perc, h] = contour(x1, x2, data1, [0 0]);
+clabel(one_perc, h)
+
 scatter(temp_GP.training_input(1:6,1), temp_GP.training_input(1:6,2));
+
+function thing = constraint(obj, x)
+            thing = predict( ...
+                obj.model(end).sweetgas_CO2_comp, ...
+                    (x - obj.values_adj.input.mean) ./ obj.values_adj.input.std ...
+                    ) * obj.values_adj.objective.std + obj.values_adj.objective.mean;
+end
