c1abc03c44f056f4e830e7d1ed07580bf1e864c7
Huang Guan <gh1514@ic.ac.uk>
2019-11-12 17:22:23 +0000

    New MATLAB files for running HYSYS case studies

---
+++ example_HYSYSFile_case_studies.m
@@ -0,0 +1,35 @@
+%% User name variables
+hysys_name = "\case study.hsc";
+spreadsheet_input = "Inputs";
+spreadsheet_output = "Outputs";
+
+%% Connect to HYSYS
+tic
+hysys_path = pwd + hysys_name;
+hysys = HYSYSFile_case_studies(hysys_path, spreadsheet_input, spreadsheet_output);
+toc
+
+%% Initialising Points for Training
+input1_steps = 17;
+input2_steps = 15;
+lb = [200, 60000];                          % Lower bounds
+ub = [1000, 200000];                        % Upper bounds
+sample_points = zeros(input1_steps*input2_steps,2);
+input1 = (lb(1):((ub(1)-lb(1))/(input1_steps-1)):ub(1));
+input2 = (lb(2):((ub(2)-lb(2))/(input2_steps-1)):ub(2));
+data_grid = zeros(input1_steps*input2_steps,27);
+
+for i = 1:input1_steps
+    sample_points(1+(i-1)*input2_steps:i*input2_steps,1) = input1(i);
+    sample_points(1+(i-1)*input2_steps:i*input2_steps,2) = input2';
+end
+
+toc
+
+for point = 1:size(sample_points, 1)
+    [outputs] = hysys.get_output(sample_points(point,:), input1_steps, point);
+    data_grid(point,:) = outputs;
+end
+
+data_grid = [sample_points, data_grid]; 
+toc
