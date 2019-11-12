e36a46ef12053944dce6901b1646cc873edfb145
Titus Ong <55629508+titus-ong@users.noreply.github.com>
2019-11-12 18:19:14 +0000

    Fix constraint plotting

--- op_region_plot_hysys_GP.m
+++ op_region_plot_hysys_GP.m
@@ -4,7 +4,7 @@ cols = 31;
 x1 = linspace(hysys.lb(1), hysys.ub(1), cols);
 x2 = linspace(hysys.lb(2), hysys.ub(2), rows);
 temp_GP = copy(GP);
-model_idx = 26;
+model_idx = 11;
 temp_GP.model = temp_GP.model(1:model_idx);
 temp_GP.values_adj = temp_GP.values_adj(1:model_idx);
 
@@ -25,15 +25,16 @@ end
 f = figure;
 hold on;
 % surf(x1, x2, data)
-%contour(x1, x2, data1,30)
+contour(x1, x2, data, 30)
 [one_perc, h] = contour(x1, x2, data1, [0 0]);
 clabel(one_perc, h)
 
 scatter(temp_GP.training_input(1:6,1), temp_GP.training_input(1:6,2));
+plot(temp_GP.centre(:, 1), temp_GP.centre(:, 2));
 
 function thing = constraint(obj, x)
             thing = predict( ...
                 obj.model(end).sweetgas_CO2_comp, ...
                     (x - obj.values_adj(end).input.mean) ./ obj.values_adj(end).input.std ...
-                    ) * obj.values_adj(end).objective.std + obj.values_adj(end).objective.mean;
+                    ) * obj.values_adj(end).sweetgas_CO2_comp.std + obj.values_adj(end).sweetgas_CO2_comp.mean;
 end
