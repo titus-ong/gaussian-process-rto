ff807529bd0048f917b1aeae0d148493d2598cd5
Titus Ong <55629508+titus-ong@users.noreply.github.com>
2019-11-13 20:15:09 +0000

    Fix syntax for calling model constraint adjusted values

--- op_region_plot_WO_GP.m
+++ op_region_plot_WO_GP.m
@@ -5,8 +5,9 @@ cols = 31;
 x1 = linspace(WO.lb(1), WO.ub(1), cols);  % Flowrate of B
 x2 = linspace(WO.lb(2), WO.ub(2), rows);  % Temp of reactor
 temp_GP = copy(GP);
-model_idx = 20;
+model_idx = 21;
 temp_GP.model = temp_GP.model(1:model_idx);
+temp_GP.values_adj = temp_GP.values_adj(1:model_idx);
 
 data = zeros(rows, cols);
 x_a = zeros(rows, cols);
@@ -14,8 +15,8 @@ x_g = zeros(rows, cols);
 for i = 1:cols
     for j = 1:rows
         data(j, i) = temp_GP.obj_fn([x1(i), x2(j)]);
-        a = predict(temp_GP.model(model_idx).x_a_con, ([x1(i), x2(j)]));
-        g = predict(temp_GP.model(model_idx).x_g_con, ([x1(i), x2(j)]));
+        a = constraint(temp_GP, 'x_a_con', ([x1(i), x2(j)]));
+        g = constraint(temp_GP, 'x_g_con', ([x1(i), x2(j)]));
         x_a(j, i) = a;
         x_g(j, i) = g;
     end
@@ -29,3 +30,10 @@ contour(x1, x2, data, 30)
 % clabel(c, h);
 [c, h] = contour(x1, x2, x_g, [0 0]);
 % clabel(c, h);
+
+function thing = constraint(obj, con, x)
+            thing = predict( ...
+                obj.model(end).(con), ...
+                    (x - obj.values_adj(end).input.mean) ./ obj.values_adj(end).input.std ...
+                    ) * obj.values_adj(end).(con).std + obj.values_adj(end).(con).mean;
+end
