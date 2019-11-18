ed953993b34b11bdd8ee01ca7a324d411ffa8755
Titus Ong <55629508+titus-ong@users.noreply.github.com>
2019-11-18 17:27:41 +0000

    Include check for points to be within upper and lower bounds

--- Pointer.m
+++ Pointer.m
@@ -4,20 +4,20 @@ classdef Pointer  < handle
         delta           % Delta of point matrix
         current_point   % Current point matrix
         dimension       % Dimensions of feasible point
+        lb              % Lower bounds
+        ub              % Upper bounds
     end
     properties (Constant)
         n_default = 5   % Number of sample points
     end
     methods
-        function obj = Pointer(feasible_point, delta)
+        function obj = Pointer(feasible_point, delta, lb, ub)
             obj.feasible_point = feasible_point;
             obj.delta = delta;
             obj.current_point = feasible_point;
             obj.dimension = length(feasible_point);
-        end
-        
-        function normaliser(obj)
-            % Normalise delta/dataset
+            obj.lb = lb;
+            obj.ub = ub;
         end
         
         function obj = update(obj, current, delta_)
@@ -41,11 +41,17 @@ classdef Pointer  < handle
             samples(1, :) = obj.current_point;
             
             % Random sampling within delta
-            for point = 2:p.Results.n+1  % Leave a space for current point
+            point = 2;
+            while point < p.Results.n+2
                 u = -1 + 2 * rand(1, obj.dimension);
                 norm = (sum(u.^2))^0.5;
                 r = rand(1, 1)^(1 / obj.dimension);
-                samples(point, :) = obj.delta .* u * r / norm + obj.current_point;
+                vec = obj.delta .* u * r / norm + obj.current_point;
+                if sum(vec > obj.ub) || sum(vec < obj.lb)
+                    continue
+                end
+                samples(point, :) = vec;
+                point = point + 1;
             end
         end
     end
