d31758157e96d47b33ef085efd2602985218577f
Huang Guan <gh1514@ic.ac.uk>
2019-11-21 00:32:09 +0000

    NEW MATLAB and HYSYS files for running fast run 3D

---
+++ example_hysys_fastrun_3D.m
@@ -0,0 +1,29 @@
+%% User name variables
+hysys_name = "\fast run - 3D.hsc";
+spreadsheet_input = "Inputs";
+spreadsheet_output = "Outputs";
+
+%% Connect to HYSYS
+tic
+hysys_path = pwd + hysys_name;
+hysys = HYSYSFile_fastrun_3D(hysys_path, spreadsheet_input, spreadsheet_output);
+toc
+
+%% Initialising Points for Training
+pointer = Pointer(hysys.feasible_point_mat, hysys.delta_mat, hysys.lb, hysys.ub);
+sample_points = pointer.random_sampling();
+[objective, con_ineq, con_eq] = hysys.get_output(sample_points);
+outputs = struct();
+outputs.objective = objective;
+outputs.con_ineq = con_ineq;
+outputs.con_eq = con_eq;
+
+GP = GPCreator(hysys, sample_points, outputs);
+GP.excite = false;
+toc
+
+%% Optimisation
+iter = 10;
+GP.optimise(iter);
+toc
+GP.plot();
