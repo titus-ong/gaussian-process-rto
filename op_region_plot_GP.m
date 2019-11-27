a404e6b374b9eed833c759b33b9ca344cd26dfab
Titus Ong <55629508+titus-ong@users.noreply.github.com>
2019-11-27 14:33:47 +0000

    Fix syntax for system

--- op_region_plot_GP.m
+++ op_region_plot_GP.m
@@ -1,10 +1,10 @@
 % Run example script first to get GP variable in workspace
 rows = 16;
 cols = 31;
-model_idx = 31;  % Which iteration of GP to display
+model_idx = 16;  % Which iteration of GP to display
 
-x1 = linspace(hysys.lb(1), hysys.ub(1), cols);
-x2 = linspace(hysys.lb(2), hysys.ub(2), rows);
+x1 = linspace(GP.lb(1), GP.ub(1), cols);
+x2 = linspace(GP.lb(2), GP.ub(2), rows);
 temp_GP = copy(GP);
 temp_GP.model = temp_GP.model(1:model_idx);
 temp_GP.values_adj = temp_GP.values_adj(1:model_idx);
@@ -12,22 +12,22 @@ temp_GP.excited = temp_GP.excited(1:model_idx);
 
 % Pre-allocation
 data = zeros(rows, cols);
-for i = 1:length(hysys.constraints_ineq)
-    val_ineq.(hysys.constraints_ineq{i}) = zeros(rows, cols);
+for i = 1:length(temp_GP.system.constraints_ineq)
+    val_ineq.(temp_GP.system.constraints_ineq{i}) = zeros(rows, cols);
 end
-for i = 1:length(hysys.constraints_eq)
-    val_eq.(hysys.constraints_eq{i}) = zeros(rows, cols);
+for i = 1:length(temp_GP.system.constraints_eq)
+    val_eq.(temp_GP.system.constraints_eq{i}) = zeros(rows, cols);
 end
 
 % GP model prediction
 for i = 1:cols
     for j = 1:rows
         data(j, i) = temp_GP.obj_fn([x1(i), x2(j)]);
-        for k = 1:length(hysys.constraints_ineq)
-            val_ineq.(hysys.constraints_ineq{k})(j, i) = constraint(temp_GP, [x1(i), x2(j)], hysys.constraints_ineq{k});
+        for k = 1:length(temp_GP.system.constraints_ineq)
+            val_ineq.(temp_GP.system.constraints_ineq{k})(j, i) = constraint(temp_GP, [x1(i), x2(j)], temp_GP.system.constraints_ineq{k});
         end
-        for k = 1:length(hysys.constraints_eq)
-            val_eq.(hysys.constraints_eq{k})(j, i) = constraint(temp_GP, [x1(i), x2(j)], hysys.constraints_eq{k});
+        for k = 1:length(temp_GP.system.constraints_eq)
+            val_eq.(temp_GP.system.constraints_eq{k})(j, i) = constraint(temp_GP, [x1(i), x2(j)], temp_GP.system.constraints_eq{k});
         end
     end
 end
@@ -38,16 +38,27 @@ hold on;
 % surf(x1, x2, data)
 contour(x1, x2, data, 30)
 
-for i = 1:length(hysys.constraints_ineq)
-    contour(x1, x2, val_ineq.(hysys.constraints_ineq{i}), [0, 0], 'k-');
+for i = 1:length(temp_GP.system.constraints_ineq)
+    contour(x1, x2, val_ineq.(temp_GP.system.constraints_ineq{i}), [0, 0], 'k-');
 end
-for i = 1:length(hysys.constraints_eq)
-    contour(x1, x2, val_eq.(hysys.constraints_eq{i}), [0, 0], 'm-');
+for i = 1:length(temp_GP.system.constraints_eq)
+    contour(x1, x2, val_eq.(temp_GP.system.constraints_eq{i}), [0, 0], 'm-');
 end
+
+% Plot last trust region
+syms x y a b h k
+a = temp_GP.delta(model_idx, 1);
+b = temp_GP.delta(model_idx, 2);
+h = temp_GP.centre(model_idx, 1);
+k = temp_GP.centre(model_idx, 2);
+ellipse = (((x-h)^2)/(a^2))+(((y-k)^2)/(b^2))==1;
+fimplicit(ellipse, [temp_GP.lb(1) temp_GP.ub(1) temp_GP.lb(2) temp_GP.ub(2)], '--b');
 
 training = scatter(temp_GP.training_input(1:6,1), temp_GP.training_input(1:6,2), '+m');
 centres = plot(temp_GP.centre(1:model_idx, 1), temp_GP.centre(1:model_idx, 2), '-b*');
 legend([centres training], {'Centres', 'Training inputs'});
+
+% Plot excited points
 if sum(temp_GP.excited)
     for i = 1:length(temp_GP.excited)
         if ~temp_GP.excited(i)
