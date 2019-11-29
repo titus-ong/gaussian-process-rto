4306e2d21a6e820f03d442f65d3a588a9f0704ba
Huang Guan <gh1514@ic.ac.uk>
2019-11-29 16:47:36 +0000

    reset reboiler duty every iteration

--- example_HYSYSFile_case_studies.m
+++ example_HYSYSFile_case_studies.m
@@ -10,10 +10,10 @@ hysys = HYSYSFile_case_studies(hysys_path, spreadsheet_input, spreadsheet_output
 toc
 
 %% Initialising Points for Training
-input1_steps = 17;
-input2_steps = 15;
-lb = [200, 60000];                          % Lower bounds
-ub = [1000, 200000];                        % Upper bounds
+input1_steps = 21;
+input2_steps = 21;
+lb = [40, 80000];                          % Lower bounds
+ub = [300, 200000];                        % Upper bounds
 sample_points = zeros(input1_steps*input2_steps,2);
 input1 = (lb(1):((ub(1)-lb(1))/(input1_steps-1)):ub(1));
 input2 = (lb(2):((ub(2)-lb(2))/(input2_steps-1)):ub(2));
@@ -27,7 +27,7 @@ end
 toc
 
 for point = 1:size(sample_points, 1)
-    [outputs] = hysys.get_output(sample_points(point,:), input1_steps, point);
+    [outputs] = hysys.get_output(sample_points(point,:));
     data_grid(point,:) = outputs;
 end
 
