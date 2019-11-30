1b6a131b323674a8fb96360e4ca369e319a31bcd
Titus Ong <55629508+titus-ong@users.noreply.github.com>
2019-11-30 18:19:36 +0000

    Update constraint tolerance

--- HYSYSFile_fastrun.m
+++ HYSYSFile_fastrun.m
@@ -72,7 +72,7 @@ classdef HYSYSFile_fastrun  < matlab.mixin.Copyable
         delta_reduction = 0.8                      % Reduction in delta when Rho < eta_low
         delta_expansion = 1.2                      % Expansion in delta when Rho > eta_high
         forgetting_factor = 1.5                    % Allowance for inaccuracies in GP due to outdated data
-        constraint_tol = 6e-4                      % Tolerance when system constraint is violated
+        constraint_tol = 4e-4                      % Tolerance when system constraint is violated
         align_tol = 5e-7                           % Tolerance of points being aligned for excitation
         region_tol = 1e-2                          % Tolerance (fraction of max TR) of points in same region for excitation
     end
